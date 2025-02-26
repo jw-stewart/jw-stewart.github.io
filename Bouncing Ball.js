9d20f235d5d700ea87c7f83d7d55440bc03767f4
jw-stewart <75709009+jw-stewart@users.noreply.github.com>
2025-02-26 09:30:54 +0900

    Update Bouncing Ball.js

--- Bouncing Ball.js
+++ Bouncing Ball.js
@@ -8,18 +8,23 @@ bouncezone.height = bouncezone.clientHeight
 const bouncezone_width = bouncezone.width
 const bouncezone_height = bouncezone.height
 
-let boxvx = 8
-let boxvy = 8
+const minX = 0
+const minY = 0
+const maxX = bouncezone_width
+const maxY = bouncezone_height
 
-let ballvx = -8
-let ballvy = -8
+let boxvx = 4
+let boxvy = 4
+
+let ballvx = -7
+let ballvy = -6
 
 
 const box = {
-    x: Math.floor(Math.random()*(1100) + 20),
-    y: Math.floor(Math.random()*(500) + 20),
     width : 80,
     height : 80,
+    x: Math.ceil(Math.random()*(bouncezone_width - 80 - 0)) + 0,
+    y: Math.ceil(Math.random()*(bouncezone_height - 80 - 0)) + 0,
     drawBox() {
         ctx.fillStyle ='#E50046'
         ctx.fillRect(this.x, this.y, this.width, this.height);
@@ -28,56 +33,69 @@ const box = {
 
 
 const ball = {
-    x: Math.floor(Math.random()*(1100) + 20),
-    y: Math.floor(Math.random()*(500) + 20),
+    x: Math.ceil(Math.random() * (bouncezone_width - 120 - 120)) + 120,
+    y: Math.ceil(Math.random() * (bouncezone_height - 120 - 120)) + 120,
+    //Math.ceil(Math.random() * (max-min)) + min 이게 공식임
     radius : 120,
     drawball() {
         ctx.beginPath();
-        ctx.arc(this.x, this.y, this.radius, 0, 2 * Math.PI);
-        ctx.strokeStyle = '#FBF3B9'
-        ctx.stroke();
+        ctx.arc(this.x, this.y, this.radius, 0, 2 * Math.PI, false);
+        ctx.fillStyle = '#FBF3B9'
+        ctx.fill();
+        ctx.closePath();
     }
     
 }
 
 function changedirection() {
-    if(box.x > bouncezone_width - box.width || box.x < 0) {
-        boxvx *= -1
-    } else if(box.y > bouncezone_height - box.height || box.y < 0) {
-        boxvy *= -1
-    } else {
-        boxvx *= 1
-        boxvy *= 1
+    if(ball.x < minX+ball.radius) {
+        ball.x = minX+ball.radius
+        ballvx *= -1
+    }
+    if(ball.x > maxX-ball.radius) {
+        ball.x = maxX-ball.radius
+        ballvx *= -1
+    }
+    if(ball.y < minY+ball.radius) {
+        ball.y = minY+ball.radius
+        ballvy *= -1
+    }
+    if(ball.y > maxY-ball.radius) {
+        ball.y = maxY-ball.radius
+        ballvy *= -1
     }
 
-    if(ball.x > bouncezone_width - ball.radius || ball.x < ball.radius) {
-        ballvx *= -1
-    } else if(ball.y > bouncezone_height - ball.radius || ball.y < ball.radius) {
-        ballvy *= -1
-    } else {
-        ballvx *= 1
-        ballvy *= 1
+    if(box.x < minX) {
+        box.x = minX
+        boxvx *= -1
+    }
+    if(box.x > maxX-box.width) {
+        box.x = maxX-box.width
+        boxvx *= -1
+    }
+    if(box.y < minY) {
+        box.y = minY
+        boxvy *= -1
+    }
+    if(box.y > maxY-box.height) {
+        box.y = maxY-box.height
+        boxvy *= -1
     }
 }
 
-// 구석에 닿았을 떄의 정의 필요
+
+
 
 function collision() {
-    if((ball.x >= box.x+box.width && ball.x <= box.x + box.width + ball.radius*2 
-        && ball.y >= box.y - box.height/2 && ball.y <= box.y + box.height*1.5)
-        || (ball.x >= box.x - ball.radius*2 && ball.x <= box.x 
-            && ball.y >= box.y - box.height/2 && ball.y <= box.y + box.height*1.5)) 
+    if(ball.x >= box.x-ball.radius && ball.x <= box.x+box.width+ball.radius
+        && ball.y >= box.y-ball.radius && ball.y <= box.y+box.height+ball.radius) 
     {
         boxvx *= -1
-        ballvx *= -1
-    } else if ((ball.y >= box.y - ball.radius*2 && ball.y <= box.y
-        && ball.x >= box.x - box.width/2 && ball.x <= box.x + box.width*1.5)
-        || (ball.y >= box.y + box.height && ball.y <= box.y + box.height + ball.radius*2
-            && ball.x >= box.x - box.width/2 && ball.x <= box.x + box.width*1.5))
-    {
         boxvy *= -1
+        ballvx *= -1
         ballvy *= -1
     }
+    
     
     else {
         boxvx *= 1
@@ -91,14 +109,17 @@ function collision() {
 
 function boxmoving() {
     ctx.clearRect(0, 0, bouncezone_width, bouncezone_height)
-    ball.drawball();
+    changedirection();
+    collision();
     ball.x += ballvx;
     ball.y += ballvy;
-    box.drawBox();
+    ball.drawball();
+    
     box.x += boxvx;
     box.y += boxvy;
-    changedirection();
-    // collision();
+    box.drawBox();
+    
+    document.querySelector('.explain_area2').innerHTML = (ball.x-120) + "/" + (ball.y-120)
     requestAnimationFrame(boxmoving)
 }
 
@@ -108,14 +129,3 @@ document.querySelector('.explain_area2').innerHTML = box.x + "/" + box.y + "/" +
 
 
 boxmoving();
-
-
-
-
-
-
-
-
-
-
-//////////////////////////////////////////////////////////////////
